8ffe946ff962fb23f178089950b246ab18035c08
Hudhayfa Jamalkhan <hudhayfajamalkhan@gmail.com>
2016-11-12 22:34:35 +0000

    Revised Game Logic

--- starter-code/main.js
+++ starter-code/main.js
@@ -5,3 +5,20 @@ var cardOne = "queen";
 var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
+
+// if else comment on paired/unpaired cards.
+if (cardOne === cardTwo) {
+alert("You found a match!");
+} else if (cardThree === cardFour){
+alert("You found a match!");
+} else if (cardOne === cardThree){
+alert("Sorry, try again");
+} else if (cardOne === cardFour){
+alert("Sorry, try again");
+} else if (cardTwo === cardThree){
+alert("Sorry, try again");
+} else if (cardTwo === cardFour){
+("Sorry, try again");
+}else {
+alert ("Sorry, try again.")
+}
